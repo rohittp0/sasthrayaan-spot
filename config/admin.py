f8bfe2d714bd9766f8970c7512402910a0f3837d
Rohit T P <tprohit9@gmail.com>
2023-01-22 15:35:38 +0530

    added queryset

--- config/admin.py
+++ config/admin.py
@@ -8,8 +8,6 @@ from config.models import GmailUser
 # Register your models here.
 
 class GmailUserAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'type')
-    list_filter = ('type', "institution")
     search_fields = ('first_name', "last_name", 'email')
     readonly_fields = ('image_tag', 'email', 'username', 'name')
 
@@ -25,6 +23,14 @@ class GmailUserAdmin(admin.ModelAdmin):
 
     image_tag.short_description = 'Image'
 
+    def group(self, user):
+        groups = []
+        for group in user.groups.all():
+            groups.append(group.name)
+            return ' '.join(groups)
+
+    group.short_description = 'Groups'
+
     def get_fieldsets(self, request, obj=None):
         if request.user.is_superuser:
             return super().get_fieldsets(request, obj)
@@ -39,9 +45,22 @@ class GmailUserAdmin(admin.ModelAdmin):
         list_display = ('name', 'email', 'type')
 
         if request.user.is_superuser:
-            return list_display + ('username', 'is_staff', 'groups', 'institution')
+            return list_display + ('username', 'is_staff', 'group', 'institution')
         else:
             return list_display + ('phone', 'institution')
+
+    def get_list_filter(self, request):
+        if request.user.is_superuser:
+            return 'is_staff', 'groups', 'type', 'institution'
+        else:
+            return 'type', "institution"
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(is_staff=False, is_superuser=False)
 
 
 admin.site.register(GmailUser, GmailUserAdmin)
