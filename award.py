5dc3399dc14cb4e1cfae85e525e6cfddbc29012e
Rohit T P <tprohit9@gmail.com>
2023-06-04 22:47:11 +0530

    auto commit

--- award.py
+++ award.py
@@ -1,7 +1,9 @@
 from config.models import GmailUser
 
-for user in GmailUser.objects.filter(institution__iexact="NSS"):
+for user in GmailUser.objects.filter(date_joined__day=24).exclude(institution__iexact="NSS"):
+    print((f"{user.first_name} {user.last_name}", user.email))
+
     user.is_staff = True
     user.groups.add(1)
     user.save()
-    
+
